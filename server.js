2957bc2ab1bd887dd6c263f87a997acc37d2c698
Chris Rico <chris.r.rico@gmail.com>
2020-06-13 13:38:18 -0700

    models input

--- server.js
+++ server.js
@@ -12,14 +12,16 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.get("/", (req, res) => {
     res.send("Hello world");
   });
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
